2470694f07737388474fc1dbe41d9c781432f0a1
pprp <1115957667@qq.com>
2024-02-15 15:45:52 +0800

    Update run_rank_gpt2.sh script and add jacobian_cosine_cost.yaml file

--- scripts/run_rank_gpt2.sh
+++ scripts/run_rank_gpt2.sh
@@ -37,11 +37,11 @@
 #      > ./logs/rank_corr_${METHOD}.log 2>&1 &
 
 # jacobian_cosine
-# METHOD=jacobian_cosine
-# CUDA_VISIBLE_DEVICES=3 python lpzero/runner/eval_rank_gpt2.py \
-#     --method $METHOD \
-#     --exp_name ./saved_logs/random_GPT2_wt103 --plot --get_cost \
-#      > ./logs/rank_corr_${METHOD}.log 2>&1 &
+METHOD=jacobian_cosine
+CUDA_VISIBLE_DEVICES=3 python lpzero/runner/eval_rank_gpt2.py \
+    --method $METHOD \
+    --exp_name ./saved_logs/random_GPT2_wt103 --plot --get_cost \
+     >> ./logs/rank_corr_${METHOD}.log 2>&1 &
 
 
 # attention_confidence 
@@ -60,16 +60,16 @@
     #  > ./logs/rank_corr_${METHOD}.log 2>&1 &
 
 # head_importance 
-METHOD=head_importance
-CUDA_VISIBLE_DEVICES=0 python lpzero/runner/eval_rank_gpt2.py \
-    --method $METHOD \
-    --exp_name ./saved_logs/random_GPT2_wt103 --plot --get_cost 
+# METHOD=head_importance
+# CUDA_VISIBLE_DEVICES=0 python lpzero/runner/eval_rank_gpt2.py \
+#     --method $METHOD \
+#     --exp_name ./saved_logs/random_GPT2_wt103 --plot --get_cost 
     # \
     #  > ./logs/rank_corr_${METHOD}.log 2>&1 &
 
 # head_confidence
-METHOD=head_confidence
-CUDA_VISIBLE_DEVICES=6 python lpzero/runner/eval_rank_gpt2.py \
-    --method $METHOD \
-    --exp_name ./saved_logs/random_GPT2_wt103 --plot --get_cost \
-     > ./logs/rank_corr_${METHOD}.log 2>&1 &
+# METHOD=head_confidence
+# CUDA_VISIBLE_DEVICES=6 python lpzero/runner/eval_rank_gpt2.py \
+#     --method $METHOD \
+#     --exp_name ./saved_logs/random_GPT2_wt103 --plot --get_cost \
+#      > ./logs/rank_corr_${METHOD}.log 2>&1 &
